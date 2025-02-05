1d670d5d7f2261db6f53dad62b8b9214f987c7e5
Kyle Racette <kracette@gmail.com>
2025-02-05 07:20:31 -0800

    Parse code, data, and memory sections

--- wasmdbg.py
+++ wasmdbg.py
@@ -1,35 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 from enum import Enum
 from dataclasses import dataclass
-
-
-def uint(b):
-    return int.from_bytes(b, 'little')
-
-
-def parse_leb128(stream):
-    num = 0
-    count = 0
-    while True:
-        byte = stream.read(1)
-        num += (uint(byte) & 127) << count
-        if uint(byte) & 128 == 0:
-            break
-        count = count + 8
-
-    return num
-
-
-def parse_name(stream):
-    length = parse_leb128(stream)
-    name = ''
-    while len(name) < length:
-        char = stream.read(1)
-        name = name + char.decode('ascii')
-
-    return name
 
 
 class SectionID(Enum):
@@ -52,10 +26,271 @@ class TypeID(Enum):
 
 
 class ValType(Enum):
-  i32 = 0x7f
-  i64 = 0x7e
-  f32 = 0x7d
-  f64 = 0x7c
+    i32 = 0x7f
+    i64 = 0x7e
+    f32 = 0x7d
+    f64 = 0x7c
+
+
+class Instruction(Enum):
+    UNREACHABLE = 0x00
+    NOP = 0x01
+    BLOCK = 0x02
+    LOOP = 0x03
+    IF = 0x04
+    ELSE = 0x05
+    # reserved space
+    END = 0x0b
+    BR = 0x0c
+    BR_IF = 0x0d
+    BR_TABLE = 0x0e
+    RETURN = 0x0f
+    CALL = 0x10
+    CALL_INDIRECT = 0x11
+    # reserved space
+    DROP = 0x1a
+    SELECT = 0x1b
+    SELECT_T = 0x1c
+    # reserved space
+    LOCAL_GET = 0x20
+    LOCAL_SET = 0x21
+    LOCAL_TEE = 0x22
+    GLOBAL_GET = 0x23
+    GLOBAL_SET = 0x24
+    TABLE_GET = 0x25
+    TABLE_SET = 0x26
+    # reserved space
+    I32_LOAD = 0x28
+    I64_LOAD = 0x29
+    F32_LOAD = 0x2a
+    F64_LOAD = 0x2b
+    I32_LOAD8_S = 0x2c
+    I32_LOAD8_U = 0x2d
+    I32_LOAD16_S = 0x2e
+    I32_LOAD16_U = 0x2f
+    I64_LOAD8_S = 0x30
+    I64_LOAD8_U = 0x31
+    I64_LOAD16_S = 0x32
+    I64_LOAD16_U = 0x33
+    I64_LOAD32_S = 0x34
+    I64_LOAD32_U = 0x35
+    I32_STORE = 0x36
+    I64_STORE = 0x37
+    F32_STORE = 0x38
+    F64_STORE = 0x39
+    I32_STORE8 = 0x3a
+    I32_STORE16 = 0x3b
+    I64_STORE8 = 0x3c
+    I64_STORE16 = 0x3d
+    I64_STORE32 = 0x3e
+    MEMORY_SIZE = 0x3f
+    MEMORY_GROW = 0x40
+    I32_CONST = 0x41
+    I64_CONST = 0x42
+    F32_CONST = 0x43
+    F64_CONST = 0x44
+    I32_EQZ = 0x45
+    I32_EQ = 0x46
+    I32_NE = 0x47
+    I32_LT_S = 0x48
+    I32_LT_U = 0x49
+    I32_GT_S = 0x4a
+    I32_GT_U = 0x4b
+    I32_LE_S = 0x4c
+    I32_LE_U = 0x4d
+    I32_GE_S = 0x4e
+    I32_GE_U = 0x4f
+    I64_EQZ = 0x50
+    I64_EQ = 0x51
+    I64_NE = 0x52
+    I64_LT_S = 0x53
+    I64_LT_U = 0x54
+    I64_GT_S = 0x55
+    I64_GT_U = 0x56
+    I64_LE_S = 0x57
+    I64_LE_U = 0x58
+    I64_GE_S = 0x59
+    I64_GE_U = 0x5a
+    F32_EQ = 0x5b
+    F32_NE = 0x5c
+    F32_LT = 0x5d
+    F32_GT = 0x5e
+    F32_LE = 0x5f
+    F32_GE = 0x60
+    F64_EQ = 0x61
+    F64_NE = 0x62
+    F64_LT = 0x63
+    F64_GT = 0x64
+    F64_LE = 0x65
+    F64_GE = 0x66
+    I32_CLZ = 0x67
+    I32_CTZ = 0x68
+    I32_POPCNT = 0x69
+    I32_ADD = 0x6a
+    I32_SUB = 0x6b
+    I32_MUL = 0x6c
+    I32_DIV_S = 0x6d
+    I32_DIV_U = 0x6e
+    I32_REM_S = 0x6f
+    I32_REM_U = 0x70
+    I32_AND = 0x71
+    I32_OR = 0x72
+    I32_XOR = 0x73
+    I32_SHL = 0x74
+    I32_SHR_S = 0x75
+    I32_SHR_U = 0x76
+    I32_ROTL = 0x77
+    I32_ROTR = 0x78
+    I64_CLZ = 0x79
+    I64_CTZ = 0x7a
+    I64_POPCNT = 0x7b
+    I64_ADD = 0x7c
+    I64_SUB = 0x7d
+    I64_MUL = 0x7e
+    I64_DIV_S = 0x7f
+    I64_DIV_U = 0x80
+    I64_REM_S = 0x81
+    I64_REM_U = 0x82
+    I64_AND = 0x83
+    I64_OR = 0x84
+    I64_XOR = 0x85
+    I64_SHL = 0x86
+    I64_SHR_S = 0x87
+    I64_SHR_U = 0x88
+    I64_ROTL = 0x89
+    I64_ROTR = 0x8a
+    F32_ABS = 0x8b
+    F32_NEG = 0x8c
+    F32_CEIL = 0x8d
+    F32_FLOOR = 0x8e
+    F32_TRUNC = 0x8f
+    F32_NEAREST = 0x90
+    F32_SQRT = 0x91
+    F32_ADD = 0x92
+    F32_SUB = 0x93
+    F32_MUL = 0x94
+    F32_DIV = 0x95
+    F32_MIN = 0x96
+    F32_MAX = 0x97
+    F32_COPYSIGN = 0x98
+    F64_ABS = 0x99
+    F64_NEG = 0x9a
+    F64_CEIL = 0x9b
+    F64_FLOOR = 0x9c
+    F64_TRUNC = 0x9d
+    F64_NEAREST = 0x9e
+    F64_SQRT = 0x9f
+    F64_ADD = 0xa0
+    F64_SUB = 0xa1
+    F64_MUL = 0xa2
+    F64_DIV = 0xa3
+    F64_MIN = 0xa4
+    F64_MAX = 0xa5
+    F64_COPYSIGN = 0xa6
+    I32_WRAP_I64 = 0xa7
+    I32_TRUNC_F32_S = 0xa8
+    I32_TRUNC_F32_U = 0xa9
+    I32_TRUNC_F64_S = 0xaa
+    I32_TRUNC_F64_U = 0xab
+    I64_EXTEND_I32_S = 0xac
+    I64_EXTEND_I32_U = 0xad
+    I64_TRUNC_F32_S = 0xae
+    I64_TRUNC_F32_U = 0xaf
+    I64_TRUNC_F64_S = 0xb0
+    I64_TRUNC_F64_U = 0xb1
+    F32_CONVERT_I32_S = 0xb2
+    F32_CONVERT_I32_U = 0xb3
+    F32_CONVERT_I64_S = 0xb4
+    F32_CONVERT_I64_U = 0xb5
+    F32_DEMOTE_F64 = 0xb6
+    F64_CONVERT_I32_S = 0xb7
+    F64_CONVERT_I32_U = 0xb8
+    F64_CONVERT_I64_S = 0xb9
+    F64_CONVERT_I64_U = 0xba
+    F64_PROMOTE_F32 = 0xbb
+    I32_REINTERPRET_F32 = 0xbc
+    I64_REINTERPRET_F64 = 0xbd
+    F32_REINTERPRET_I32 = 0xbe
+    F64_REINTERPRET_F64 = 0xbf
+    I32_EXTEND8_S = 0xc0
+    I32_EXTEND16_S = 0xc1
+    I64_EXTEND8_S = 0xc2
+    I64_EXTEND16_S = 0xc3
+    I64_EXTEND32_S = 0xc4
+    # reserved
+    REF_NULL = 0xd0
+    REF_IS_NULL = 0xd1
+    REF_FUNC = 0xd2
+    # reserved
+
+
+def uint(b):
+    return int.from_bytes(b, 'little')
+
+
+def parse_leb128(stream):
+    num = 0
+    count = 0
+    while True:
+        byte = stream.read(1)
+        num += (uint(byte) & 127) << count
+        if uint(byte) & 128 == 0:
+            break
+        count = count + 7
+
+    return num
+
+
+def parse_name(stream):
+    length = parse_leb128(stream)
+    name = ''
+    while len(name) < length:
+        char = stream.read(1)
+        name = name + char.decode('ascii')
+
+    return name
+
+
+def parse_expression(stream):
+    expr = []
+    instr = None
+    while True:
+        # TODO: handle other const types
+        if instr == Instruction.I32_CONST:
+            value = parse_leb128(stream)
+            expr.append(value)
+            instr = None
+        else:
+            instr = Instruction(uint(stream.read(1)))
+            expr.append(instr)
+            if instr == Instruction.END:
+                break
+            
+    return expr
+    
+
+class CountStream:
+    def __init__(self, stream):
+        self.stream = stream
+        self.size = self._file_length(stream)
+        self.start()
+    
+    def _file_length(self, stream):
+        stream.seek(0, os.SEEK_END)
+        size = stream.tell()
+        stream.seek(0, os.SEEK_SET) 
+        return size
+
+    def start(self):
+        self.count = 0
+        
+    def read(self, num_bytes):
+        b = self.stream.read(num_bytes) 
+        self.count += len(b)
+        return b
+    
+    def tell(self):
+        return self.stream.tell()
 
 
 class Function:
@@ -100,7 +335,6 @@ class Export:
 
     def parse(self, stream):
         self.name = parse_name(stream)
-        print(self.name)
 
         export_type = uint(stream.read(1))
         if export_type == 0:
@@ -118,14 +352,16 @@ class Local:
     num: int
     typ: ValType
 
+
 class Code:
     def __init__(self):
         self.size = None
         self.locals = []
-        self.code = None
+        self.instructions = []
 
     def parse(self, stream):
         self.size = parse_leb128(stream)
+        stream_count = stream.count
 
         num_locals = parse_leb128(stream)
         while len(self.locals) < num_locals:
@@ -134,9 +370,34 @@ class Code:
 
             self.locals.append(Local(num=num_of_type, typ=typ))
 
-        # TODO: read into `self.code` until we have read `self.size` bytes,
-        # including those read for the locals.
-        # There should be a `instr.END` to end the code block.
+        prev_instr = None
+        while stream.count < stream_count + self.size:
+
+            if prev_instr == Instruction.I32_CONST:
+                value = parse_leb128(stream)
+                self.instructions.append(value)
+                prev_instr = None
+            # TODO: handle other `const` instructions
+            else:
+                instr = Instruction(uint(stream.read(1)))
+                self.instructions.append(instr)
+                prev_instr = instr
+                
+
+class Data:
+    def __init__(self):
+        self.memidx = None
+        self.expr = None
+        self.data = None
+        
+    def parse(self, stream):
+        self.memidx = parse_leb128(stream) 
+        self.expr = parse_expression(stream)
+        
+        num_bytes = parse_leb128(stream)
+        self.data = stream.read(num_bytes)
+        # for d in self.data:
+        #   print(f'{hex(d)} - {chr(d)}')
 
 
 class Section:
@@ -148,10 +409,13 @@ class Section:
         pass
 
     def parse(self, stream):
+        stream.start()
         assert self.size >= 0
         if self.size == 0:
             return
-        return self._parse(stream)
+        result = self._parse(stream)
+        assert stream.count == self.size, f'{stream.count} == {self.size}'
+        return result
 
     def _parse(self, stream):
         raise Exception('Not implemented')
@@ -163,7 +427,6 @@ class TypeSection(Section):
 
     def _parse(self, stream):
         num_funcs = parse_leb128(stream)
-        print(f'  num funcs: {num_funcs}')
 
         for idx in range(num_funcs):
             type_id = TypeID(uint(stream.read(1)))
@@ -178,7 +441,6 @@ class TypeSection(Section):
 class ImportSection(Section):
     def _parse(self, stream):
         num_imports = parse_leb128(stream)
-        print(f'  num imports: {num_imports}')
 
         for idx in range(num_imports):
             # Not yet supported
@@ -191,7 +453,6 @@ class FunctionSection(Section):
 
     def _parse(self, stream):
         num_funcs = parse_leb128(stream)
-        print(f'  num functions: {num_funcs}')
 
         while len(self.func_idx) < num_funcs:
             func_idx = parse_leb128(stream)
@@ -204,7 +465,6 @@ class MemorySection(Section):
 
     def _parse(self, stream):
         num_mems = parse_leb128(stream)
-        print(f'  num memories: {num_mems}')
 
         while len(self.memories) < num_mems:
             mem = Memory()
@@ -219,7 +479,6 @@ class ExportSection(Section):
 
     def _parse(self, stream):
         num_exports = parse_leb128(stream)
-        print(f'  num exports: {num_exports}')
 
         while len(self.exports) < num_exports:
             export = Export()
@@ -234,13 +493,26 @@ class CodeSection(Section):
 
     def _parse(self, stream):
         num_codes = parse_leb128(stream)
-        print(f'  num codes: {num_codes}')
 
         while len(self.codes) < num_codes:
             code = Code()
             code.parse(stream)
 
             self.codes.append(code)
+
+
+class DataSection(Section):
+    def _init(self):
+        self.datas = []
+
+    def _parse(self, stream):
+        num_datas = parse_leb128(stream)
+        
+        while len(self.datas) < num_datas:
+            print(f'Data section {len(self.datas)}')
+            data = Data()
+            data.parse(stream)
+            self.datas.append(data)
 
 
 def _section_from_id(sec_id):
@@ -251,6 +523,8 @@ def _section_from_id(sec_id):
             SectionID.FUNCTION: FunctionSection,
             SectionID.MEMORY: MemorySection,
             SectionID.EXPORT: ExportSection,
+            SectionID.CODE: CodeSection,
+            SectionID.DATA: DataSection
         }[sec_id]
     except KeyError:
         raise Exception(f'Bad section ID: {sec_id}')
@@ -275,6 +549,10 @@ class Module:
 
     def _parseSections(self, stream):
         while True:
+            if stream.tell() == stream.size:
+                # end of input
+                return
+
             print('parsing section...')
             sec_id_bytes = stream.read(1)  # 1 byte
             sec_id = SectionID(uint(sec_id_bytes))
@@ -299,7 +577,7 @@ def main():
 
     mod = Module()
     with open(filename, 'rb') as fp:
-        mod.parse(fp)
+        mod.parse(CountStream(fp))
 
     print('WebAssembly module')
     print(f'  version: {mod.version}')
